6fd3209a26788e6f5b3627bb091b4f614ffebd9c
bryanbermudez <bryan06jb@gmail.com>
2025-11-12 17:05:22 -0600

    IMC para calcular el peso

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import Edad from "./src/Views/Promedio.js";
 import Productos from "./src/Views/Productos.js";
 import Empleados from "./src/Views/Empleados.js";
 import ProductosRealtime from "./src/Views/ProductosRealtime.js";
+import IMCCalculator from "./src/Views/CalcularIMC.js";
 
 export default function App() {
 
-  return <ProductosRealtime />;
+  return <Productos />;
 }
